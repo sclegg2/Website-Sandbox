713a3e864db01416d9f6226bf817c19ea9c439bb
sclegg2 <sclegg2@unl.edu>
2019-01-19 18:59:30 -0600

    add scripts for downloading raw source code

--- getScripty.js
+++ getScripty.js
@@ -12,3 +12,34 @@ function runCode() {
 }
 
 runCode();
+
+function downloadCode(filename, content) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
+document.getElementById("htmlDownloadButton").addEventListener("click", function(){
+    var content = document.getElementById("html").value;
+    var filename = "test.html";
+
+    downloadCode(filename, content);
+}, false);
+
+document.getElementById("cssDownloadButton").addEventListener("click", function(){
+    var content = document.getElementById("css").value;
+    var filename = "test.css";
+
+    downloadCode(filename, content);
+}, false);
+
+document.getElementById("jsDownloadButton").addEventListener("click", function(){
+    var content = document.getElementById("js").value;
+    var filename = "test.js";
+
+    downloadCode(filename, content);
+}, false);
